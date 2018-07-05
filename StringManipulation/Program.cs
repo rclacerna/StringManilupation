49e87ef33c6c4376ae70b8a7ed7c14981fbb64f8
rclacerna <rclacerna@gmail.com>
2018-07-05 15:03:57 +0100

    Fizzbuzz and reverse string

--- StringManipulation/Program.cs
+++ StringManipulation/Program.cs
@@ -5,10 +5,12 @@ namespace StringManipulation
 {
     class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
-             Compress();
-             JewelsAndStones();
+            Compress();
+            JewelsAndStones();
+            ReverseString();
+            FizzBuzz();
         }
 
         public static void Compress()
@@ -20,7 +22,7 @@ namespace StringManipulation
             int current_index;
             int count = 1;
 
-            for (current_index = 0; current_index < input.Length-1; current_index++)
+            for (current_index = 0; current_index < input.Length - 1; current_index++)
             {
                 var current_letter = input[current_index];
                 var next_letter = input[current_index + 1];
@@ -46,12 +48,49 @@ namespace StringManipulation
             var stones = "aaAbbbb";
             var count = 0;
 
-    	    foreach (var stone in stones)
+            foreach (var stone in stones)
                 foreach (var jewel in jewels)
-                    if(stone == jewel) count++;
+                    if (stone == jewel) count++;
 
-            System.Console.WriteLine("Output: "+ count);
+            System.Console.WriteLine("Output: " + count);
         }
 
+        public static void ReverseString()
+        {
+            System.Console.WriteLine("-----------REVERSE STRING----------");
+            var s = "hello";
+            char right_temp;
+            var output = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                right_temp = s[(s.Length - 1) - i];
+                output += right_temp;
+            }
+            System.Console.WriteLine("reverse: " + output);
+        }
+
+        public static void FizzBuzz()
+        {
+            var input = 15;
+
+            for (int i = 0; i <= input; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    System.Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                }
+                else
+                    System.Console.WriteLine(i);
+            }
+        }
     }
 }
